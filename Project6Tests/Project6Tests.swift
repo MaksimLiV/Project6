77da70844affb998f6fdf8cb1035971fc8b2d93f
Maksim Li <maximlee.ml@gmail.com>
2024-10-27 21:47:37 +0200

    Initial Commit

---
+++ Project6Tests/Project6Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Project6Tests.swift
+//  Project6Tests
+//
+//  Created by Maksim Li on 26/10/2024.
+//
+
+import Testing
+@testable import Project6
+
+struct Project6Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Project6UITests/Project6UITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Project6UITestsLaunchTests.swift
+//  Project6UITests
+//
+//  Created by Maksim Li on 26/10/2024.
+//
+
+import XCTest
+
+final class Project6UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
